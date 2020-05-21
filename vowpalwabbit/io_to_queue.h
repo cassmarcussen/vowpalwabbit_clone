422ec3239b38195bb03196ccc0702b4b3f039765
Cassandra Marcussen <cassandramarcussencello@gmail.com>
2020-05-20 21:05:38 -0400

    Adding io_to_queue queue creation and pop methods

--- vowpalwabbit/io_to_queue.h
+++ vowpalwabbit/io_to_queue.h
@@ -60,4 +60,80 @@ class IO_Item {
 
 #endif
 
+static std::queue<IO_Item> *input_lines_copy = new std::queue<IO_Item>;
+static bool called_i_l_t = false;
+static bool have_added_io = false;
+static bool done_with_io = false;
+
+inline void io_lines_toqueue(vw *all, std::queue<IO_Item> *input_lines){
+
+  //lock -- results in error reduction, but gets rid of ability to access input_lines before the function finishes
+  std::lock_guard<std::mutex> lck(_mutex_io);
+
+  parser *original_p = all->p;
+  
+  char* line = nullptr;
+
+  while(true)
+  {
+
+    size_t num_chars_initial = readto(*(all->p->input), line, '\n');
+   
+    if(num_chars_initial < 1 || strlen(line) < 1){
+        called_i_l_t = true;
+        have_added_io = true;
+        break;
+    }
+
+    IO_Item line_item(std::string(line), num_chars_initial);
+
+    input_lines->push(line_item);
+    input_lines_copy->push(line_item);
+
+
+    called_i_l_t = true;
+    have_added_io = true;
+
+  }
+
+  all->p = original_p;
+  done_with_io = true;
+
+}
+
+inline bool added_io(){
+  return have_added_io;
+}
+
+inline bool io_done(){
+  return done_with_io;
+}
+
+//will hopefully be called after io_lines_to_queue called.
+inline IO_Item pop_io_queue(bool should_pop){
+
+
+  std::lock_guard<std::mutex> lck(_mutex_io);
+
+  if(should_pop && input_lines_copy->size() > 0)
+  {
+    IO_Item front = input_lines_copy->front();
+
+    //std::cout << "pop line from io queue (when item in)" << std::endl;
+
+    input_lines_copy->pop();
+
+    return front;
+    
+  }else if(input_lines_copy->size() == 0 && have_added_io && done_with_io){
+    //std::cout << "pop line from io queue (before item in)" << std::endl;
+    return IO_Item(std::string("empty"), 0);
+  }else if(input_lines_copy->size() == 0 && have_added_io && !done_with_io){
+    return IO_Item(std::string("bad"), 0);
+  }
+
+  return IO_Item(std::string("bad"), 0);
+
+}
+
 #endif
